b0b2f495e223e284def3c912883c70fce11a522c
exposit <forgepoet@gmail.com>
2015-11-01 15:30:17 -0600

    Create slacktoken.py

---
+++ slacktoken.py
@@ -0,0 +1,9 @@
+# create a bot integration and follow the instructions on the slack website
+# to post as bot, use bot's auth token; to post as a user, use a user auth token
+token = ""
+
+# set default channel id for the bot to post in, by name or id
+channel = "roleplaying"
+
+# set the gamebot's id so it can respond properly (not utilized right now)
+botid = ""
